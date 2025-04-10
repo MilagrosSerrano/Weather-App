6b4d0d24f19b4bafe963866a130556d1b7bb7404
MilagrosSerrano <serranomilagros23@gmail.com>
2025-04-10 09:44:57 -0300

    change on the welcome-app

--- src/screens/WelcomeScreen/WelcomeApp.jsx
+++ src/screens/WelcomeScreen/WelcomeApp.jsx
@@ -1,6 +1,6 @@
 import { FooterApp } from "../../footer/FooterApp";
 import "./WelcomeApp.css";
-<<<<<<< HEAD
+
 import polarBearImage from "../../assets/polar-bear.png";
 export const WelcomeApp = () => {
   return (
@@ -12,26 +12,12 @@ export const WelcomeApp = () => {
       </h2>
       <p className="description">
         We don't know about styling, but we could help you by telling you if you
-        need a coat or not!{" "}
+        need a coat or not!
       </p>
       <p className="description">
         Just type the name of the place where you live, we'll take care of the
         rest ☺️
       </p>
-=======
-export const WelcomeApp = () => {
-  return (
-    <div className="welcomeApp">
-      <img
-        src="src/assets/polar-bear.png"
-        alt="polar-bear"
-        className="polarBear"
-      />
-      <h1 id="welcomeTitle">Welcome to Weather Wizards</h1>
-      <h2 id="subtitle">Are you tired of not picking the right clothes for the day?</h2>
-      <p className="description">We don't know about styling, but we could help you by telling you if you need a coat or not! </p>
-      <p className="description">Just type the name of the place where you live, we'll take care of the rest ☺️</p>
->>>>>>> 8e9fae4d720862fc8c582b9f8bfd1ed96a237ddf
       <button className="weatherCheck">Check the weather</button>
       <FooterApp></FooterApp>
     </div>
