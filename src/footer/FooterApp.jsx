b997f757f83779551fc3693df0a040b15dd04fbe
MilagrosSerrano <serranomilagros23@gmail.com>
2025-04-24 01:59:09 -0300

    change on footer

--- src/footer/FooterApp.jsx
+++ src/footer/FooterApp.jsx
@@ -3,7 +3,7 @@ import './Footer.css'
 export const FooterApp = () =>{
     return(
     <footer className="footer-app">
-     <p>Powered by OpenWeatherMap and OpenMeteo</p>
+     <p>Powered by API Ninjas and Open-meteo</p>
     </footer>
     )
 }
